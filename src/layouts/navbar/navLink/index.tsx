c6b490951bb6640a721fe18de72b30dbb737ed03
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-18 19:52:16 +0200

    refactor: setup <NavLink> component

--- src/layouts/navbar/navLink/index.tsx
+++ src/layouts/navbar/navLink/index.tsx
@@ -1,10 +1,14 @@
-import './index.css';
+import type { NavLinkType } from "../../../types/navbar.types.ts/NavLinkType";
+import "./index.css";
 
-const NavLink = () => {
-    return(
-        <div className='nav-link'>
-            
-        </div>
-    )
-}
+const NavLink = ({ icon, reddirect }: NavLinkType) => {
+  const { id, name, svg } = icon;
+
+  return (
+    <li className="nav-link" onClick={() => reddirect(name)}>
+      {svg}
+      <a id={id}>{name}</a>
+    </li>
+  );
+};
 export default NavLink;
