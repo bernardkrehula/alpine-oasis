78d14bf6669c2ee8eda99e0ac3e8187a800fce47
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-20 20:02:37 +0200

    feat: add handleActiveIcon function to <Header> component

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,27 +1,55 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import Icon from "./Icon";
+import type { ActiveIconType } from "../../types/header.types.ts/ActiveIconType";
+import { USER } from "../../config/userConfig";
 
 const Header = () => {
   const [theme, setTheme] = useState<boolean>(false);
-
+  const [activeIcon, setActiveIcon] = useState<ActiveIconType>({
+    user: false,
+    blackTheme: false,
+    whiteTheme: false,
+    logout: false,
+  });
   const changeTheme = () => {
     setTheme((prev) => !prev);
+  };
+  const handleActiveIcon = (e: React.MouseEvent<HTMLLIElement>) => {
+    const name = e.currentTarget.dataset.name;
+    setActiveIcon((prev) => {
+      const active = Object.fromEntries(
+        Object.entries(prev).map(([icon]) =>
+          icon === name ? [name, true] : [icon, false],
+        ),
+      );
+      return active as typeof activeIcon;
+    });
   };
 
   return (
     <header className="main-header">
-      <img className="profile-photo" src="profilePhoto.png" alt="" />
-      <h1>Elena Novak</h1>
-      <div className="icons">
-        <Icon name="user" variation="secondary"/>
-        {theme ? (
-          <Icon name="black$theme" onClick={changeTheme} variation="secondary"/>
-        ) : (
-          <Icon name="white$theme" onClick={changeTheme} variation="secondary"/>
-        )}
-        <Icon name="logout" variation="secondary"/>
-      </div>
+      <img className="profile-photo" src={USER.photo} alt="" />
+      <h1>{USER.name}</h1>
+      <ul className="icons">
+        <li data-name="user" onClick={handleActiveIcon}>
+          <Icon name="user" variation="secondary" activeIcon={activeIcon} />
+        </li>
+        <li
+          data-name={`${!theme ? "whiteTheme" : "blackTheme"}`}
+          onClick={handleActiveIcon}
+        >
+          <Icon
+            name={`${theme ? "whiteTheme" : "blackTheme"}`}
+            onClick={changeTheme}
+            activeIcon={activeIcon}
+            variation="secondary"
+          />
+        </li>
+        <li data-name="logout" onClick={handleActiveIcon}>
+          <Icon name="logout" activeIcon={activeIcon} variation="secondary" />
+        </li>
+      </ul>
     </header>
   );
 };
