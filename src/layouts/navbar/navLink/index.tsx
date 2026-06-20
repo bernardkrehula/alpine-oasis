a22faa5894220fafa14b7c5fc50fbca2540e70d6
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-20 20:03:13 +0200

    feat: pass activeLink to <NavLink> component

--- src/layouts/navbar/navLink/index.tsx
+++ src/layouts/navbar/navLink/index.tsx
@@ -1,11 +1,11 @@
 import type { NavLinkType } from "../../../types/navbar.types.ts/NavLinkType";
 import "./index.css";
 
-const NavLink = ({ icon, reddirect }: NavLinkType) => {
+const NavLink = ({ icon, reddirect, activeLink }: NavLinkType) => {
   const { id, name, svg } = icon;
 
   return (
-    <li className="nav-link" onClick={() => reddirect(name)}>
+    <li className={`nav-link ${activeLink[name] && 'active-link'}`} data-name={name} onClick={reddirect}>
       {svg}
       <a id={id}>{name}</a>
     </li>
