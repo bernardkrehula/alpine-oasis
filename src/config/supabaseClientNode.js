dd8d43179b37c57057483f28fc9b2e62f35e0b68
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-24 17:10:34 +0200

    chore: set supabaseClientNode.js file

---
+++ src/config/supabaseClientNode.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseKey = process.env.VITE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
