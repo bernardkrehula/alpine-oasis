0f254eb3e601f8dcf3012f3ae092a23eac7baa9a
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-28 20:22:25 +0200

    feat: add sortConfig file

---
+++ src/config/configData/sortConfig.ts
@@ -0,0 +1,26 @@
+export const apartmentsSort = [
+  { name: "name-asc", content: "Sort by name (A-Z)" },
+  { name: "name-desc", content: "Sort by name (Z-A)" },
+  { name: "price-asc", content: "Sort by price (low first)" },
+  { name: "price-desc", content: "Sort by price (high first)" },
+  { name: "capacity-asc", content: "Sort by capacity (low first)" },
+  { name: "capacity-desc", content: "Sort by capacity (high first)" },
+];
+
+export const aparmnetsDiscount = [
+  {
+    id: "all",
+    name: "all",
+    content: "all",
+  },
+  {
+    id: 'noDiscount',
+    name: "no-discount",
+    content: "No discount",
+  },
+  {
+    id: 'withDiscount',
+    name: "with-discount",
+    content: "with discount",
+  },
+];
