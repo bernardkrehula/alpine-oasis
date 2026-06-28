f9e1b6a2068f490a77b2db6c6f64a13f7f0d0638
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-28 20:17:20 +0200

    refactor: pass new properties to <ApartmentMenu> component

--- src/pages/apartments/apartment/apartmentMenu/index.tsx
+++ src/pages/apartments/apartment/apartmentMenu/index.tsx
@@ -1,13 +1,24 @@
 import { HiOutlineDuplicate } from "react-icons/hi";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
-
 import "./index.css";
+import type { ApartmentMenuType } from "#/types/pagest.types.ts/ApartmentPage.types.ts/ApartmentMenu.type";
+import { useRef } from "react";
+import { useClickOutside } from "#/hooks/useClickOutside";
 
-const ApartmentMenu = ({ id, handleActiveEditModal }) => {
+const ApartmentMenu = ({
+  id,
+  handleActiveEditModal,
+  deleteApartment,
+  duplicateApartment,
+  onClickOutside,
+}: ApartmentMenuType) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useClickOutside(ref, onClickOutside);
+
   return (
-    <div className="apartment-menu">
-      <div>
+    <div className="apartment-menu" id={id} ref={ref}>
+      <div onClick={duplicateApartment}>
         <HiOutlineDuplicate />
         <span>Duplicate</span>
       </div>
@@ -15,7 +26,7 @@ const ApartmentMenu = ({ id, handleActiveEditModal }) => {
         <FaPencil />
         <span>Edit</span>
       </div>
-      <div>
+      <div onClick={() => deleteApartment(id)}>
         <FaRegTrashAlt />
         <span>Delete</span>
       </div>
