31983e1380b9a2c14b341143070d3f2029899f73
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-28 20:25:13 +0200

    refactor: add react router and tanstack query to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import router from './routes'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import router from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} /> 
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />{" "}
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
