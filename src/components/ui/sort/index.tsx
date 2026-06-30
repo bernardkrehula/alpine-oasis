23488a23c12d0f091d283ccffe33167700b269c4
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-30 22:11:45 +0200

    refactor: rename <Select> to <Sort> component

---
+++ src/components/ui/sort/index.tsx
@@ -0,0 +1,15 @@
+import type { SelectType } from "#/types/navbar.types.ts/SelectType";
+import "./index.css";
+import Option from "./option";
+
+const Sort = ({ options, size, onChange }: SelectType) => {
+  return (
+    <select onChange={onChange} className={`select ${size}`}>
+      {options.map((option) => {
+        const {name, content} = option;
+        return <Option name={name} content={content} />;
+      })}
+    </select>
+  );
+};
+export default Sort;
