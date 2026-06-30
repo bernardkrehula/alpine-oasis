bf7ab1d18ec5ac7dcef7576a0f6754f4c86c91b6
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-30 22:09:35 +0200

    feat: add apartmentsTableData file

---
+++ src/pages/apartments/apartmentsTableData.ts
@@ -0,0 +1,8 @@
+export const theadData = [
+  { className: "apartment-th", name: "Apartment" },
+  { className: "capacity-th", name: "Capacity" },
+  { className: "price-th", name: "Price" },
+  { className: "discount-th", name: "Discount" },
+];
+
+
