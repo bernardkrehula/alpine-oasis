a921933ad39922353dc3eb891eed0d3eac5ad797
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-04 22:57:57 +0200

    refactor: change <TableHeader> component structure

--- src/components/ui/table/TableHeader/index.tsx
+++ src/components/ui/table/TableHeader/index.tsx
@@ -1,10 +1,21 @@
-import './index.css';
+import "./index.css";
 
-const TableHeader = () => {
-    return(
-        <th>
-            
-        </th>
-    )
-}
+const TableHeader = ({ theadData, difference }) => {
+  return (
+    <tr>
+      {theadData.map((row) => {
+        const { name, className } = row;
+
+        return (
+          <th key={name} className={className}>
+            <span>{name}</span>
+          </th>
+        );
+      })}
+      {Array.from({ length: difference }).map((_, index) => {
+        return <th key={index}></th>;
+      })}
+    </tr>
+  );
+};
 export default TableHeader;
