d0ba9a2c41459eb5140b9f35827daf33cc5bd900
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-04 22:58:57 +0200

    feat: add <EditTableMenu> component with terinary operator to <TableData> component

--- src/components/ui/table/TableData/index.tsx
+++ src/components/ui/table/TableData/index.tsx
@@ -1,16 +1,34 @@
+import { HiDotsVertical } from "react-icons/hi";
+import Btn from "../../btn";
 import "./index.css";
+import EditTableMenu from "#/pages/apartments/apartment/apartmentMenu";
+import { useState } from "react";
 
-const TableData = ({ row, columns }) => {
-    console.log(row)
+const TableData = ({ row, columns, onSecondAction, onThirdAction }) => {
+  const [activeMenu, setActiveMenu] = useState<boolean>(false);
+  const handleActiveMenu = () => {
+    setActiveMenu((prev) => !prev);
+  };
+
   return (
     <tr>
       {columns.map((col) => {
-        return (
-          <td key={col.key} className={col.className}>
-            {col.render ? col.render(row) : row[col.key]}
-          </td>
-        );
+        if (col.render) return <td>{col.render(row)}</td>;
       })}
+      <td className="menu-btn" onClick={handleActiveMenu}>
+        <Btn type="button" variation="ghost-dark" size="lg">
+          <HiDotsVertical />
+        </Btn>
+        {activeMenu && (
+          <EditTableMenu
+            id={row.id}
+            onFirstAction={handleActiveMenu}
+            onThirdAction={onThirdAction}
+            onSecondAction={onSecondAction}
+            onClickOutside={handleActiveMenu}
+          />
+        )}
+      </td>
     </tr>
   );
 };
