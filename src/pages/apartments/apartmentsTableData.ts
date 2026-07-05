3bcb5c43038f08005ab73df312c0019572077757
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-05 19:28:39 +0200

    chore: remove unused files and code

--- src/pages/apartments/apartmentsTableData.ts
+++
@@ -1,8 +0,0 @@
-export const theadData = [
-  { className: "apartment-th", name: "Apartment" },
-  { className: "capacity-th", name: "Capacity" },
-  { className: "price-th", name: "Price" },
-  { className: "discount-th", name: "Discount" },
-];
-
-
--- src/types/pagest.types.ts/ApartmentPage.types.ts/ApartmentMenu.type.ts
+++
@@ -1,7 +0,0 @@
-export type ApartmentMenuType = {
-  id: string;
-  handleActiveEditModal: () => void;
-  deleteApartment: (value: string) => void;
-  duplicateApartment: () => void;
-  onClickOutside: () => void;
-};
