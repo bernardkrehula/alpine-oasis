0a1b2d830c37ab3f0f97e46fb9c2b8ad05a71bde
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-12 20:43:09 +0200

    chore: remove unnecessary code

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />{" "}
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 );
