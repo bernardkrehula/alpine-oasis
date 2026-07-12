1cff25dc26b5ecb2e0cf89f4ce395e39b63c7774
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-12 20:29:44 +0200

    feat: add <PublicRoutes> component

---
+++ src/utils/PublicRoutes.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import type { Session } from "@supabase/supabase-js";
+import supabase from "#/config/supabaseClientVite";
+
+const PublicRoutes = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session);
+      setIsLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (isLoading) return null;
+
+  return session ? <Navigate to="/apartments" replace /> : <Outlet />;
+};
+export default PublicRoutes;
