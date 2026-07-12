5f103072e7fd09438d84aee9d640548a3b63fbfb
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-12 20:43:56 +0200

    fix: change icon property name svg to path

--- src/layouts/navbar/navLink/index.tsx
+++ src/layouts/navbar/navLink/index.tsx
@@ -1,12 +1,16 @@
-import type { NavLinkType } from "../../../types/navbar.types.ts/NavLinkType";
+import type { NavLinkType } from "#/types/navbar.types.ts/NavLinkType";
 import "./index.css";
 
 const NavLink = ({ icon, reddirect, activeLink }: NavLinkType) => {
-  const { id, name, svg } = icon;
+  const { id, name, path } = icon;
 
   return (
-    <li className={`nav-link ${activeLink[name] && 'active-link'}`} data-name={name} onClick={reddirect}>
-      {svg}
+    <li
+      className={`nav-link ${activeLink[name as keyof typeof activeLink] && "active-link"}`}
+      data-name={name}
+      onClick={reddirect}
+    >
+      {path}
       <a id={id}>{name}</a>
     </li>
   );
